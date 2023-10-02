4ef56bbafa8cea18e66e4a71ede583cb6ff5f1e6
Katie F <kmfreerksen@gmail.com>
2023-10-01 22:42:47 -0500

    Step 7

--- chessnotes/lib/views/verify_email_view.dart
+++ chessnotes/lib/views/verify_email_view.dart
@@ -1,3 +1,4 @@
+import 'package:chessnotes/constants/routes.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -17,14 +18,24 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
         ),
       body: Column(
         children: [
-          const Text('Please verify your email address:'),
+          const Text("We've sent you an email verification. Please open it and click the link to verify your account."),
+          const Text("If you haven't received a verification email yet, press the button below."),
           TextButton(
             onPressed: () async {
               final user = FirebaseAuth.instance.currentUser;
               await user?.sendEmailVerification();
             }, 
-            child: const Text('Send email verification')
+            child: const Text('Resend email verification')
           ),
+          TextButton(onPressed: () async {
+            await FirebaseAuth.instance.signOut();
+            Navigator.of(context).pushNamedAndRemoveUntil(
+              registerRoute, 
+              (route) => false,
+            );
+          }, 
+          child: const Text('Restart'),
+          )
         ],
       ),
     );
