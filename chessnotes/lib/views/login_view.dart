9db82648a98a6b81f431dcfd0c9021cc9bc0a857
Katie F <kmfreerksen@gmail.com>
2023-10-01 20:31:38 -0500

    clean up print statements

--- chessnotes/lib/views/login_view.dart
+++ chessnotes/lib/views/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -61,15 +62,15 @@ class _LoginViewState extends State<LoginView> {
                   email: email,
                   password: password,
                 );
-                print(userCredential);
+                devtools.log(userCredential.toString());
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
-                  print('User not found');
+                  devtools.log('User not found');
                 } else if (e.code == 'INVALID_LOGIN_CREDENTIALS') {
-                  print('Invalid Logon Credentials');
+                  devtools.log('Invalid Logon Credentials');
                 } else {
-                  print('SOMETHING ELSE HAPPENED');
-                  print(e.code);
+                  devtools.log('SOMETHING ELSE HAPPENED');
+                  devtools.log(e.code);
                 }
               }
             }, 
--- chessnotes/lib/views/register_view.dart
+++ chessnotes/lib/views/register_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -63,16 +64,16 @@ class _RegisterViewState extends State<RegisterView> {
                     email: email, 
                     password: password,
                   );
-              print(userCredential);
+              devtools.log(userCredential.toString());
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'weak-password') {
-                  print('Weak Password. Must be at least 6 characters.');
+                  devtools.log('Weak Password. Must be at least 6 characters.');
                 } else if (e.code == 'email-already-in-use') {
-                  print('Email is already in use.');
+                  devtools.log('Email is already in use.');
                 } else if (e.code == 'invalid-email') {
-                  print('Ivalid Email entered.');
+                  devtools.log('Ivalid Email entered.');
                 } else {
-                  print(e.code);
+                  devtools.log(e.code);
                 }
               }                  
             }, 
